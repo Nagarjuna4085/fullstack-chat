5bd5f12457cb1db59b41ec7fc52f9d1dab0e4431
nagarjnasanam <snagarjuna2001@gmail.com>
2025-12-20 04:57:21 -0500

    frontend UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import HomePage from "./pages/HomePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
 export default function App() {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
-  console.log({ authUser });
+  console.log({ onlineUsers });
 
   if (isCheckingAuth && !authUser)
     return (
@@ -22,7 +28,9 @@ export default function App() {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
+      <Navbar />
+
       <Routes>
         <Route
           path="/"
