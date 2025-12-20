5f05b2c1fbe48ac5e287c7c29c3f9644054b2fa6
nagarjnasanam <snagarjuna2001@gmail.com>
2025-12-20 07:01:32 -0500

    store images in cloudinary

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -3,6 +3,10 @@ import { v2 as cloudinary } from "cloudinary";
 import { config } from "dotenv";
 
 config();
+console.log("Cloudinary config:");
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+console.log("API Secret:", process.env.CLOUDINARY_API_SECRET ? "LOADED" : "MISSING");
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
